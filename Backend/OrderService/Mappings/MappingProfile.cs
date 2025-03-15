9cf25450073ac1b5297e865305ee87c780fdfe7c
Ahmed-Fa1sal <DerRiese765@gmail.com>
2025-03-15 14:24:16 +0300

    fixing order list

--- Backend/OrderService/Mappings/MappingProfile.cs
+++ Backend/OrderService/Mappings/MappingProfile.cs
@@ -2,11 +2,22 @@ using OrderService.DTOs;
 using OrderService.Models;
 using AutoMapper;
 
-public class MappingProfile : Profile {
-    public MappingProfile() {
-        CreateMap<CreateOrderDto, Order>();
-        CreateMap<UpdateOrderDto, Order>();
-        CreateMap<Order, OrderDto>();
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Map item collections
         CreateMap<OrderItemDto, OrderItem>();
+        CreateMap<OrderItem, OrderItemDto>();
+        
+        // Handle nested collections in these mappings
+        CreateMap<CreateOrderDto, Order>()
+            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items));
+            
+        CreateMap<UpdateOrderDto, Order>()
+            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items));
+            
+        CreateMap<Order, OrderDto>()
+            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items));
     }
 }
--- Frontend/ecommerce-app/src/app/components/order-list/order-list.component.ts
+++ Frontend/ecommerce-app/src/app/components/order-list/order-list.component.ts
@@ -2,18 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { loadOrders } from '../../store/order/order.actions';
-import { selectAllOrders } from '../../store/order/order.selectors';
+import { selectAllOrders, selectOrderError, selectOrderLoading } from '../../store/order/order.selectors';
 
 @Component({
   standalone: true,
   imports: [CommonModule],
+  selector: 'app-order-list',
   templateUrl: './order-list.component.html',
 })
 export class OrderListComponent {
   orders$ = this.store.select(selectAllOrders);
-  
+  loading$ = this.store.select(selectOrderLoading); // Fixed selector
+  error$ = this.store.select(selectOrderError);     // Fixed selector
 
   constructor(private store: Store) {
-    this.store.dispatch(loadOrders());
+    this.store.dispatch(loadOrders()); // Dispatch action to load orders
   }
 }
